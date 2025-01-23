f5826daf71222e665d76d6e6a559d17c8d6753ae
Leandro Prado <125649970+leprado06@users.noreply.github.com>
2025-01-23 18:34:13 -0300

    first commit

---
+++ Boas Vindas.rb
@@ -0,0 +1,45 @@
+Boas Vindas
+    Olá, seja muito bem vindo(a) ao meu portifólio de projetos de Ciência de Dados.
+    
+    Nessa pagina, eu demonstro minhas habilidades de resolver problemas de negócio utilizando conceitos e ferramentas da
+    Ciência de Dados, através de projetos com dados públicos.
+
+    Você vai encontrar também, minhas experiências profissionais, habilidades, ferramentas e conceitos envolvendo a 
+    Ciência de Dados.
+
+    Sinta-se à vontade para entrar em contato através dos links no final da página.
+
+Sobre mim
+    Meu nome é Leandro Prado, tenho formação em contabilidade e experiência como micro emprésario. Atualmente estudando e
+    aprofundando conhecimento na área de Ciência de Dados.
+
+    No meu dia a dia como dono de loja, gerencio equipe e demandas de clientes. Sendo necessário tomada de decisões rápidas
+    e pontuais. Organização de multitarefas e planejamento de tempo é primordial para que todo o processo de negócio
+    seja executado de forma eficiente.
+
+    Na busca por novos desafios e retomando um desejo de toda uma vida, decidi iniciar minha jornada como Cientista de Dados,
+    tenho me dedicado todos os dias à aprender mais sobre a profissão. Assim, busco uma oportunidade para iniciar na carreira
+    dentro de uma companhia séria e que possa contribuir com informações pontuais para melhorar a tomada de decisão dentro da
+    empresa.
+
+Habilidades
+    Linguagens de programação e Banco de dados
+    Estatística e Machine Learning
+    Visualização de Dados
+    Engenharia de software
+
+Experiências
+    Projeto de Ciência de Dados
+    
+    5+ Anos como micro empresário.
+        Dono de comércio liderando equipe de 3 à 6 pessoas, atendendo alta demanda de pedidos, organização de tarefas e
+        prioridades, pensamento análitico e planejamento de tempo. Implementação de processos novos, treinamento de equipe
+        e tomadas de decisão pontuais.
+    7+ anos como contabilista.
+        Funcionário de grandes organizações na área contábil, domínio de conceitos contábeis e financeiros, experiência com 
+        Fisco no Brasil e diversos softwares de contabilidade. Implementação de sistemas tributários e contábeis e treinamento
+        de outros colaboradores.
+
+
+
+    
